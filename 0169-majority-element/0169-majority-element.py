f3cf53eb0493306e20320e1249c2193aa24610b4
Corinne <Corinnekogut@gmail.com>
2024-01-31 14:26:38 -0500

    Time: 140 ms (57.29%), Space: 17.9 MB (74.60%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         dict1 = {}
-        majority_count = 0
-        majority_key = None
+        majority_count = 1
+        majority_key = nums[0]
         for i in nums:
             if i not in dict1:
                 dict1[i] = 1
-                if  dict1.get(i, 0) > majority_count:
-                    majority_count = dict1[i]
-                    majority_key = i
             else:
                 dict1[i]+=1
                 if  dict1.get(i, 0) > majority_count:
