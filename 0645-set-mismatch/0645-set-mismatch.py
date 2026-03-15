9c8b32e50a0e9af285def6fa75840bcada1d7ce7
Corinne <Corinnekogut@gmail.com>
2026-03-15 17:24:05 -0400

    0645-SET-MISMATCH Stats: Time: 3 ms (95.74%), Space: 13.3 MB (93.45%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = [0,0]                #[repeated number, missing number]
 
-        for i in range(len(nums)):
-            count = nums.count(i+1)
-            if count == 2:
-                result[0] = i+1
-            if count == 0:
-                result[1] = i+1
+        countList = [0] * (len(nums))     #list of place holding 0s
+        result = [0,0]                      #[repeated number, missing number]
+
+        for i in range(len(nums)):      
+            value = nums[i]
+            countList[value-1] += 1
             
+        result[0] = countList.index(2)+1
+        result[1] = countList.index(0)+1
         return result
