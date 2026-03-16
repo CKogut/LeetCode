2aad7b87eac29f4cf3084643eac55f45f6d47a09
Corinne <Corinnekogut@gmail.com>
2026-03-16 08:47:11 -0400

    1482-HOW-MANY-NUMBERS-ARE-SMALLER-THAN-THE-CURRENT-NUMBER Stats: Time: 3 ms (85.47%), Space: 12.3 MB (94.47%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        result = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[i]>nums[j]:
-                    count +=1
-            result.append(count)
+        sorted_nums = sorted(nums)      # create sorted list
+                                        # the index of the first occurence of a number tells you how many
+                                        # other numbers are smaller 
+        counts = {}                     # create dictionary to store counts of smaller numbets
+
+        for index, num in enumerate(sorted_nums):
+            if num not in counts:     # ignore repeated numbers
+                counts[num] = index     # captures number as key and the index the value
+        result = [counts[num] for num in nums]
         return result
